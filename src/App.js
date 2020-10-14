67ceaa55e20310ee091784433cb322af196568b6
Brian Lockerbie <brian.lockerbie@gmail.com>
2020-10-13 23:42:20 -0400

    Created Hero.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { RecipesState } from "./context/RecipesContext";
 import { BrowswerRouter as Router, Route } from "react-router-dom";
 import Nav from './components/Nav';
 import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <Nav />
-    </Router>
+    <RecipesState>
+      <Router>
+        <Nav />
+      </Router>
+    </RecipesState> 
   );
 };
 
